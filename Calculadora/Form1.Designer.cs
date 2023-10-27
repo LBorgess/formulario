d5133c75b20418887f9d0fee162504184426357f
LBorgess <lucas_rancer@hotmail.com>
2023-10-27 19:37:08 -0300

    Botões da calculadora

--- Calculadora/Form1.Designer.cs
+++ Calculadora/Form1.Designer.cs
@@ -29,35 +29,108 @@ namespace Calculadora
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtn1 = new System.Windows.Forms.TextBox();
+            this.txtn2 = new System.Windows.Forms.TextBox();
+            this.btnsomar = new System.Windows.Forms.Button();
+            this.btnsubtrair = new System.Windows.Forms.Button();
+            this.btnmultiplicar = new System.Windows.Forms.Button();
+            this.btndivisao = new System.Windows.Forms.Button();
+            this.lblresultado = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // textBox1
+            // label1
             // 
-            this.textBox1.AcceptsReturn = true;
-            this.textBox1.AcceptsTab = true;
-            this.textBox1.AutoCompleteCustomSource.AddRange(new string[] {
-            "Amazonas",
-            "Bahia",
-            "Rio de Janeiro",
-            "São Paulo",
-            "Santa Catarina",
-            "Vitória",
-            "Belo Horizonte"});
-            this.textBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
-            this.textBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
-            this.textBox1.Location = new System.Drawing.Point(172, 65);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(408, 277);
-            this.textBox1.TabIndex = 0;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(258, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1º NUM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(502, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "2º NUM";
+            // 
+            // txtn1
+            // 
+            this.txtn1.Location = new System.Drawing.Point(236, 128);
+            this.txtn1.Name = "txtn1";
+            this.txtn1.Size = new System.Drawing.Size(100, 20);
+            this.txtn1.TabIndex = 3;
+            // 
+            // txtn2
+            // 
+            this.txtn2.Location = new System.Drawing.Point(480, 128);
+            this.txtn2.Name = "txtn2";
+            this.txtn2.Size = new System.Drawing.Size(100, 20);
+            this.txtn2.TabIndex = 4;
+            // 
+            // btnsomar
+            // 
+            this.btnsomar.Location = new System.Drawing.Point(252, 216);
+            this.btnsomar.Name = "btnsomar";
+            this.btnsomar.Size = new System.Drawing.Size(75, 23);
+            this.btnsomar.TabIndex = 5;
+            this.btnsomar.Text = "+";
+            this.btnsomar.UseVisualStyleBackColor = true;
+            // 
+            // btnsubtrair
+            // 
+            this.btnsubtrair.Location = new System.Drawing.Point(252, 265);
+            this.btnsubtrair.Name = "btnsubtrair";
+            this.btnsubtrair.Size = new System.Drawing.Size(75, 23);
+            this.btnsubtrair.TabIndex = 6;
+            this.btnsubtrair.Text = "-";
+            this.btnsubtrair.UseVisualStyleBackColor = true;
+            // 
+            // btnmultiplicar
+            // 
+            this.btnmultiplicar.Location = new System.Drawing.Point(490, 216);
+            this.btnmultiplicar.Name = "btnmultiplicar";
+            this.btnmultiplicar.Size = new System.Drawing.Size(75, 23);
+            this.btnmultiplicar.TabIndex = 7;
+            this.btnmultiplicar.Text = "x";
+            this.btnmultiplicar.UseVisualStyleBackColor = true;
+            // 
+            // btndivisao
+            // 
+            this.btndivisao.Location = new System.Drawing.Point(490, 265);
+            this.btndivisao.Name = "btndivisao";
+            this.btndivisao.Size = new System.Drawing.Size(75, 23);
+            this.btndivisao.TabIndex = 8;
+            this.btndivisao.Text = "/";
+            this.btndivisao.UseVisualStyleBackColor = true;
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Location = new System.Drawing.Point(386, 352);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(35, 13);
+            this.lblresultado.TabIndex = 9;
+            this.lblresultado.Text = "label3";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.btndivisao);
+            this.Controls.Add(this.btnmultiplicar);
+            this.Controls.Add(this.btnsubtrair);
+            this.Controls.Add(this.btnsomar);
+            this.Controls.Add(this.txtn2);
+            this.Controls.Add(this.txtn1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -67,7 +140,15 @@ namespace Calculadora
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtn1;
+        private System.Windows.Forms.TextBox txtn2;
+        private System.Windows.Forms.Button btnsomar;
+        private System.Windows.Forms.Button btnsubtrair;
+        private System.Windows.Forms.Button btnmultiplicar;
+        private System.Windows.Forms.Button btndivisao;
+        private System.Windows.Forms.Label lblresultado;
     }
 }
 
