dd7aec0f3547b6f277c9884fe7b560cc73df986f
LBorgess <lucas_rancer@hotmail.com>
2023-11-10 19:33:47 -0300

    Form: Nome completo

--- NovoFormulário/Form1.Designer.cs
+++ NovoFormulário/Form1.Designer.cs
@@ -29,13 +29,46 @@ namespace NovoFormulário
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbNomeCompleto = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbNomeCompleto
+            // 
+            this.lbNomeCompleto.AutoSize = true;
+            this.lbNomeCompleto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNomeCompleto.Location = new System.Drawing.Point(29, 45);
+            this.lbNomeCompleto.Name = "lbNomeCompleto";
+            this.lbNomeCompleto.Size = new System.Drawing.Size(120, 20);
+            this.lbNomeCompleto.TabIndex = 0;
+            this.lbNomeCompleto.Text = "Nome completo";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(403, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbNomeCompleto);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbNomeCompleto;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
