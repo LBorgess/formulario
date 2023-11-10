ab9602208a81be768be32516634eb565008bc8bf
LBorgess <lucas_rancer@hotmail.com>
2023-11-10 19:42:32 -0300

    Masked

--- NovoFormulário/Form1.Designer.cs
+++ NovoFormulário/Form1.Designer.cs
@@ -30,7 +30,9 @@ namespace NovoFormulário
         private void InitializeComponent()
         {
             this.lbNomeCompleto = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtNomeCompleto = new System.Windows.Forms.TextBox();
+            this.lblCPF = new System.Windows.Forms.Label();
+            this.mCPF = new System.Windows.Forms.MaskedTextBox();
             this.SuspendLayout();
             // 
             // lbNomeCompleto
@@ -43,19 +45,41 @@ namespace NovoFormulário
             this.lbNomeCompleto.TabIndex = 0;
             this.lbNomeCompleto.Text = "Nome completo";
             // 
-            // textBox1
+            // txtNomeCompleto
             // 
-            this.textBox1.Location = new System.Drawing.Point(33, 68);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(403, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtNomeCompleto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNomeCompleto.Location = new System.Drawing.Point(33, 68);
+            this.txtNomeCompleto.Name = "txtNomeCompleto";
+            this.txtNomeCompleto.Size = new System.Drawing.Size(403, 26);
+            this.txtNomeCompleto.TabIndex = 1;
+            // 
+            // lblCPF
+            // 
+            this.lblCPF.AutoSize = true;
+            this.lblCPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCPF.Location = new System.Drawing.Point(29, 104);
+            this.lblCPF.Name = "lblCPF";
+            this.lblCPF.Size = new System.Drawing.Size(40, 20);
+            this.lblCPF.TabIndex = 2;
+            this.lblCPF.Text = "CPF";
+            // 
+            // mCPF
+            // 
+            this.mCPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mCPF.Location = new System.Drawing.Point(33, 132);
+            this.mCPF.Mask = "000,000,000-00";
+            this.mCPF.Name = "mCPF";
+            this.mCPF.Size = new System.Drawing.Size(403, 26);
+            this.mCPF.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 461);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.mCPF);
+            this.Controls.Add(this.lblCPF);
+            this.Controls.Add(this.txtNomeCompleto);
             this.Controls.Add(this.lbNomeCompleto);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -68,7 +92,9 @@ namespace NovoFormulário
         #endregion
 
         private System.Windows.Forms.Label lbNomeCompleto;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtNomeCompleto;
+        private System.Windows.Forms.Label lblCPF;
+        private System.Windows.Forms.MaskedTextBox mCPF;
     }
 }
 
