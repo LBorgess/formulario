2f4593a2268f9a499432f5767dd56125f3b3ee34
LBorgess <lucas_rancer@hotmail.com>
2023-11-10 19:48:50 -0300

    Lista de estados

--- NovoFormulário/Form1.Designer.cs
+++ NovoFormulário/Form1.Designer.cs
@@ -33,6 +33,8 @@ namespace NovoFormulário
             this.txtNomeCompleto = new System.Windows.Forms.TextBox();
             this.lblCPF = new System.Windows.Forms.Label();
             this.mCPF = new System.Windows.Forms.MaskedTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblEstado = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lbNomeCompleto
@@ -72,11 +74,59 @@ namespace NovoFormulário
             this.mCPF.Size = new System.Drawing.Size(403, 26);
             this.mCPF.TabIndex = 3;
             // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Acre",
+            "Alagoas\t",
+            "Amapá\t",
+            "Amazonas\t",
+            "Bahia\t",
+            "Ceará\t",
+            "Espírito Santo\t",
+            "Goiás\t",
+            "Maranhão",
+            "Mato Grosso\t",
+            "Mato Grosso do Sul",
+            "Minas Gerais",
+            "Pará\t",
+            "Paraíba\t",
+            "Paraná\t",
+            "Pernambuco\t",
+            "Piauí\t",
+            "Rio de Janeiro",
+            "Rio Grande do Norte\t",
+            "Rio Grande do Sul",
+            "Rondônia",
+            "Roraima",
+            "Santa Catarina\t",
+            "São Paulo\t",
+            "Sergipe\t",
+            "Tocantins"});
+            this.comboBox1.Location = new System.Drawing.Point(34, 200);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(402, 28);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEstado.Location = new System.Drawing.Point(29, 175);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(168, 20);
+            this.lblEstado.TabIndex = 5;
+            this.lblEstado.Text = "Estado de nascimento";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.mCPF);
             this.Controls.Add(this.lblCPF);
             this.Controls.Add(this.txtNomeCompleto);
@@ -95,6 +145,8 @@ namespace NovoFormulário
         private System.Windows.Forms.TextBox txtNomeCompleto;
         private System.Windows.Forms.Label lblCPF;
         private System.Windows.Forms.MaskedTextBox mCPF;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblEstado;
     }
 }
 
