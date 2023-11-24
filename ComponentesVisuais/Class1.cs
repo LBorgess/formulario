807e9bdb68a952e8f157a38fb7ae20b99e14073a
LBorgess <lucas_rancer@hotmail.com>
2023-11-24 19:37:09 -0300

    Class Estado

---
+++ ComponentesVisuais/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComponentesVisuais
+{
+    public class Estado
+    {
+        public string Sigla { get; set; }
+        public string Nome { get; set; }
+
+        public Estado(string Sigla, string Nome)
+        {
+            this.Sigla = Sigla;
+            this.Nome = Nome;
+        }
+
+
+    }
+}
