b4d8f935e8ff7acb3a518b35665c849ca6592893
LBorgess <lucas_rancer@hotmail.com>
2023-11-27 21:19:35 -0300

    Criação da classe estado

---
+++ NovoFormulário/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovoFormulário
+{
+    class Estado
+    {
+        public string sigla { get; set; }
+        public string nome { get; set; }
+
+        public Estado(string sigla, string nome)
+        {
+            this.sigla = sigla;
+            this.nome = nome; 
+        }
+    }
+}
